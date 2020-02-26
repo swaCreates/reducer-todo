5733d6261dec2df02294b8ac44821a8ebd71f5f1
Francois Chery <swa_isthecreator@Francoiss-MBP.home>
2020-02-26 15:24:10 -0500

    cleaning working environment

---
+++ reducer-todo-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Reducer-To-do-App</h1>
+    </div>
+  );
+}
+
+export default App;
